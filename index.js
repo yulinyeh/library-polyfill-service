0a72eebcaebb8e90a287e954c146ed43030f846f
yulinyeh <caco99@gmail.com>
2018-09-13 15:57:13 +0800

    更新 IE 所需 API

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require('polyfill-service').getPolyfillString({
   uaString: 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
   minify: true,
   features: {
-    'default': {},
-    'Array.prototype.find': {},
     'String.prototype.repeat': {},
+    'Number.isNaN': {},
+    'Array.prototype.find': {},
     'Object.entries': {},
-    'Object.assign': {}
+    'Object.assign': {},
+    'Math.trunc': {},
+    'EventSource': {}
   }
 }).then(function(_bundleString) {
   fs.writeFile('./polyfill.min.js', _bundleString, _error => {
